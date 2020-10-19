e84ce47970e7f86329d4cbb79a5785e4ba55e828
User <User@DESKTOP-6N6EHH6>
2020-10-19 21:58:45 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan4/kambing/PBOIF210119067Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan2.deklarasivariabel;
+
+/**
+ *
+ *
+ * NAMA     : Andreas Danhago
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi Program : Deklarasi & Pengaksesan
+ */
+public class PBOIF210119067Latihan1Biodata 
+ public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBOIF210119082Latihan4Kambing kambingJantan = new PBOIF210119082Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
